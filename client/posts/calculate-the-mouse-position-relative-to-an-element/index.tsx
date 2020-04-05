2d0473a838d9265bc9785aa435977d96b41bd6e7
小石头 <XUHHEMAIL@163.COM>
2020-04-05 21:14:01 +0800

    calculate the mouse position relative to an element文章翻译

--- client/posts/calculate-the-mouse-position-relative-to-an-element/index.tsx
+++ client/posts/calculate-the-mouse-position-relative-to-an-element/index.tsx
@@ -12,17 +12,17 @@ export default () => {
 </Helmet>
 <Markdown
     content={`
-The following code calculates the mouse position relative to the clicked element:
+以下代码计算鼠标相对于 \`clicked\` 元素的位置: 
 
 ~~~ javascript
 ele.addEventListener('mousedown', function(e) {
-    // Get the target
+    // 获取到 target 属性
     const target = e.target;
 
-    // Get the bounding rectangle of target
+    // 从 target 中获取到元素的大小
     const rect = target.getBoundingClientRect();
 
-    // Mouse position
+    // 鼠标的相对位置
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 });
