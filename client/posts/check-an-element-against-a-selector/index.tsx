35da512becf63ee73a1d445b90ad0198c77a4a76
小石头 <XUHHEMAIL@163.COM>
2020-04-06 22:24:50 +0800

    check an element against a selector 文章翻译

--- client/posts/check-an-element-against-a-selector/index.tsx
+++ client/posts/check-an-element-against-a-selector/index.tsx
@@ -9,7 +9,7 @@ export default () => {
 <Markdown
     content={`
 ~~~ javascript
-// Return true if \`ele\` matches the CSS selector \`selector\`
+// 如果 \`ele\` 元素中存在与传入的 \`selector\` 相匹配的 CSS 选择器，则返回 true
 const matches = function(ele, selector) {
     return (
         ele.matches || 
@@ -21,6 +21,8 @@ const matches = function(ele, selector) {
     ).call(ele, selector);
 };
 ~~~
+
+相关链接: [Element.matches()](https://developer.mozilla.org/zh-CN/docs/Web/API/Element/matches)
 `}
 />
 <RelatedPosts
